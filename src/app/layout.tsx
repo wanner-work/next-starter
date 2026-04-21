82c055eb0b58934ada0d330075ef4587c7455cc9
Jonas Wanner <jonas.wanner@clavisit.com>
2026-04-21 09:38:23 +0200

    init: adding next starter initialization

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: 'Zubi - Schuhe, Bekleidung und Ausrüstung für Draussen'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="de"
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        'font-sans',
+        inter.variable
+      )}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="font-bold">next starter</h1>
+    </main>
+  )
+}
